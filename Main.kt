dc899244450a80693b5054e327ede842a7b31b7b
shorouksaffan <shorouk.saffan2005@gmail.com>
2025-07-22 14:38:45 +0300

    Update Main.kt
    Extracted a print function

--- Main.kt
+++ Main.kt
@@ -1,19 +1,22 @@
 //Creating a data class Task
 data class Task(val title: String, val isDone: Boolean)
-
-fun main() {
-    //Declaring a mutable list of tasks using val and adding 3 sample tasks
-    val tasks= mutableListOf(Task("submit assignment", true),
-        Task("clean the house", false),
-        Task("study", true))
-
-    //Looping over each task and printing status
-    for (task in tasks) {
-        if (task.isDone) {
+//Function to check whether the task is done or not rather than the loop
+fun printTask(task: Task) {
+     if (task.isDone) {
             println("✅ ${task.title}")
         } 
         else {
             println("❌ ${task.title}")
         }
+}
+
+fun main() {
+   //Declaring a mutable list of tasks using val and adding 3 sample tasks
+   val tasks= mutableListOf(Task("submit assignment", true),
+        Task("clean the house", false),
+        Task("study", true))
+//Looping through each task and print its status using printTask()
+    for (task in tasks) {
+        printTask(task)
     }
 }
